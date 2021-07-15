cbf6295ce8bd37c4d64cb657075c3c12830d7851
Atsushi Koshiba <cos24boc@gmail.com>
2021-07-15 19:06:13 +0200

    fixed hash value of musl_lib

--- cmake/cross_compiled_libraries.cmake
+++ cmake/cross_compiled_libraries.cmake
@@ -19,7 +19,7 @@ else(BUNDLE_LOC)
 	ExternalProject_Add(PrecompiledLibraries
 			    PREFIX precompiled
 			    URL https://github.com/hioa-cs/IncludeOS/releases/download/v0.12.0-rc.2/IncludeOS_dependencies_v0-12-0_musl_libunwind_singlethreaded.tar.gz
-          URL_HASH SHA1=51e59475d1a94d7b4aedf7b894397015d1dc048e
+			    URL_HASH SHA1=d011b393fff5eba6df865ffb085628a105e9404d
 			    CONFIGURE_COMMAND ""
 			    BUILD_COMMAND ""
 			    UPDATE_COMMAND ""
@@ -36,7 +36,7 @@ ExternalProject_Add(solo5_repo
   # GIT_TAG 285b80aa4da12b628838a78dc79793f4d669ae1b
   GIT_REPOSITORY git@github.com:AtsushiKoshiba/funky-solo5.git
   # GIT_TAG vfpga # branch name is also ok
-  GIT_TAG 0ddbe93cb1fe19c6a017e59b861bb8b2a5e9afc6
+  GIT_TAG 51e59475d1a94d7b4aedf7b894397015d1dc048e
 	CONFIGURE_COMMAND CC=gcc ./configure.sh
 	UPDATE_COMMAND ""
   BUILD_COMMAND make DISABLE_FPGA=${SOLO5_DISABLE_FPGA}
