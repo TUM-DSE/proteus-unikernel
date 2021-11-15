f70fa4e88dd3c7cf54348a378f1e002229d72a30
Atsushi Koshiba <cos24boc@gmail.com>
2021-11-15 14:49:19 +0100

    eval: update count_loc.sh

--- funky-scripts/evaluation/count_loc.sh
+++ funky-scripts/evaluation/count_loc.sh
@@ -16,6 +16,7 @@ count_loc() {
     echo "count ${app}..."
     cd ${arg_dir}/${app}
 
+    LOG="${EVAL_SCRIPT_ROOT}/${DIR}/${arg_apps[$i]}.log"
     CSV="${EVAL_SCRIPT_ROOT}/${DIR}/${arg_apps[$i]}.csv"
 
     HOSTCODE="host.cpp"
@@ -23,21 +24,41 @@ count_loc() {
     if [ ${app} = "simple_vadd" ]; then
       HOSTCODE="vadd.cpp"
     elif [ ${arg_dir} = ${ROSETTA_DIR} ]; then
-      HOSTCODE=${ROSETTA_CODE[$i]}
+      HOSTCODE=""
       HOSTCODE_DIR="src/host"
     fi
 
     COMMIT=`git rev-parse HEAD` # get commit hash
     echo "${apps}, ${COMMIT}" >> ${CSV}
-    cloc --quiet ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/*.h
-    cloc --quiet ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/*.h --csv >> ${CSV}
+    cloc --quiet ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/*.h --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file | tee ${LOG}
+    cloc --quiet ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/*.h --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file --csv >> ${CSV}
 
     echo "code changes for Funky" >> ${CSV}
-    cloc --quiet --diff ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_dir}/${app}/${HOSTCODE}
-    cloc --diff --quiet ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_dir}/${app}/${HOSTCODE} --csv >> ${CSV}
+    cloc --quiet --diff ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_dir}/${app}/${HOSTCODE} --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file | tee ${LOG}
+    cloc --quiet --diff ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_dir}/${app}/${HOSTCODE} --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file --csv >> ${CSV}
   done
-}
 
+  # additional count for common lib
+  if [ ${arg_dir} = ${VITIS_EXAMPLES_DIR} ]; then
+    LOG="${EVAL_SCRIPT_ROOT}/${DIR}/funky_utils.log"
+    CSV="${EVAL_SCRIPT_ROOT}/${DIR}/funky_utils.csv"
+
+    cloc --quiet ${arg_orig_dir}/../common/includes --exclude-dir=oclHelper,opencl,simplebmp --by-file | tee ${LOG}
+    cloc --quiet ${arg_orig_dir}/../common/includes --exclude-dir=oclHelper,opencl,simplebmp --by-file --csv >> ${CSV}
+
+    cloc --quiet --diff ${arg_orig_dir}/../common/includes ${arg_dir}/../funky_utils --exclude-dir=oclHelper,opencl,simplebmp --exclude-list-file=timer.h --by-file | tee ${LOG}
+    cloc --quiet --diff ${arg_orig_dir}/../common/includes ${arg_dir}/../funky_utils --exclude-dir=oclHelper,opencl,simplebmp --exclude-list-file=timer.h --by-file --csv >> ${CSV}
+  else 
+    LOG="${EVAL_SCRIPT_ROOT}/${DIR}/harness.log"
+    CSV="${EVAL_SCRIPT_ROOT}/${DIR}/harness.csv"
+
+    cloc --quiet ${arg_orig_dir}/harness/ocl_src --by-file | tee ${LOG}
+    cloc --quiet ${arg_orig_dir}/harness/ocl_src --by-file --csv >> ${CSV}
+
+    cloc --quiet --diff ${arg_orig_dir}/harness/ocl_src ${arg_dir}/harness --by-file | tee ${LOG}
+    cloc --quiet --diff ${arg_orig_dir}/harness/ocl_src ${arg_dir}/harness --by-file --csv >> ${CSV}
+  fi
+}
 
 DIR="loc_$DATE"
 mkdir -p $DIR
