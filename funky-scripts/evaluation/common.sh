364e573cec8d01706e351245c5a3f8d48dceb37e
Atsushi Koshiba <cos24boc@gmail.com>
2021-11-18 20:23:37 +0100

    eval: update measure_time.sh for repeated exection, minor changes for shift_register
    measure_time.py, which is called by measure_time.sh,
    repeats the execution of each workload a specified number of times.
    
    A list of workloads and input arguments is given as a CSV file.
    See vitis_applist.csv or rosetta_applist.csv.

--- funky-scripts/evaluation/common.sh
+++ funky-scripts/evaluation/common.sh
@@ -1,23 +1,25 @@
 DATE=`date "+%m%d%Y_%H%M%S"`
 
+##### directory paths
 EVAL_SCRIPT_ROOT=$(readlink -f $(dirname ${BASH_SOURCE:-$0}))
 VITIS_EXAMPLES_DIR=${EVAL_SCRIPT_ROOT}/../../examples/Vitis_Accel_Examples
 ROSETTA_DIR=${EVAL_SCRIPT_ROOT}/../../examples/Rosetta
 
+##### params for unikernel execution
 UKVM_BIN=${INCLUDEOS_PREFIX}/includeos/x86_64/lib/ukvm-bin
 TAP_IF=tap100
 SOCKET_IF=/tmp/solo5_socket
 MIG_FILE=mig_file
-# APP_BIN=build/funkycl-app
+APP_BIN=build/funkycl-app
+UKVM_EXEC_CMD="${UKVM_BIN} --mem=1024 --disk=${APP_BIN} --net=${TAP_IF} ${MON_OPT} ${LOAD_OPT} ${APP_BIN} ${APP_BIN}"
 
+##### Vitis_Accel_Examples
 VITIS_EXAMPLES_APPS=(array_partition axi_burst_performance bind_op_storage burst_rw critical_path custom_datatype dataflow_stream dataflow_stream_array dependence_inter kernel_chain lmem_2rw loop_pipeline loop_reorder partition_cyclicblock port_width_widening shift_register simple_vadd systolic_array wide_mem_rw)
-
 VITIS_EXAMPLES_ARGS=("matmul.xclbin" "-x1 ./test_kernel_maxi_256bit.xclbin -x2 ./test_kernel_maxi_512bit.xclbin" "vadd.xclbin" "vadd.xclbin" "-x apply_watermark_GOOD.xclbin -i xilinx_img.bmp -c golden.bmp" "-x rgb_to_hsv.xclbin -i xilinx_logo.bmp" "adder.xclbin" "N_stage_Adders.xclbin" "vconv.xclbin" "krnl_mmult.xclbin" "vadd.xclbin" "vector_addition.xclbin" "mmult.xclbin" "matmul.xclbin" "krnl_port_widen.xclbin" "fir.xclbin" "krnl_vadd.xclbin" "mmult.xclbin" "vadd.xclbin")
 
+##### Rosetta
 ROSETTA_APPS=(3d-rendering digit-recognition optical-flow spam-filter)
-
 ROSETTA_ARGS=("-f rendering.xclbin" "-f DigitRec.hw.xclbin" "-f optical_flow.hw.xclbin -p sintel_alley -o outputs.flo" "-f SgdLR.hw.xclbin -p data/")
-
 ROSETTA_CODE=("3d_rendering_host.cpp" "digit_recognition.cpp" "optical_flow_host.cpp" "spam_filter.cpp")
 
 find_binary() {
@@ -36,7 +38,7 @@ find_binary() {
     exit -1
   fi
 
-  echo "${APP_BIN} is found."
+  # echo "${APP_BIN} is found."
 }
 
 set_ukvm_permission() {
@@ -65,6 +67,14 @@ measure_ukvm_exec_time() {
   # /usr/bin/time -o out.csv -f '%e, %S, %U, %P' ls
 }
 
+measure_ukvm_exec_time_py() {
+  arg_dir=$1
+  arg_out_csv=$2
+  arg_app_list=$3
+  arg_repeat=$4
+  arg_cmd=${@:5}
 
+  python3 ${EVAL_SCRIPT_ROOT}/measure_time.py ${arg_dir} ${arg_out_csv} ${arg_app_list} ${arg_repeat} ${arg_cmd}
+}
 
 
--- funky-scripts/evaluation/measure_time.sh
+++ funky-scripts/evaluation/measure_time.sh
@@ -2,37 +2,64 @@
 
 source ./common.sh
 
-measure_time() {
-  arg_dir=$1
-  arg_apps_array=$2[@]
-  arg_apps=("${!arg_apps_array}")
-  arg_args_array=$3[@]
-  arg_args=("${!arg_args_array}")
-  
+function usage {
+  cat <<EOF
 
-  DIR="time_$DATE"
-  mkdir -p ${EVAL_SCRIPT_ROOT}/$DIR
+Usage: 
+  $(basename ${0}) <repeat>
 
-  # evaluation for Vitis_Accel_Examples
-  for i in ${!arg_apps[@]}; do
-    echo "execute ${arg_apps[$i]}..."
-    cd ${arg_dir}/${arg_apps[$i]}
-    find_binary "build/"
-  
-    CSV="${EVAL_SCRIPT_ROOT}/${DIR}/${arg_apps[$i]}.csv"
-    LOG="${EVAL_SCRIPT_ROOT}/${DIR}/${arg_apps[$i]}.log"
-    USER_ARGS=${arg_args[$i]}
-  
-    # execute app
-    measure_ukvm_exec_time ${CSV} ${LOG} ${USER_ARGS}
-  done 
+  Measure the end-to-end execution time of workloads in Vitis_Accel_Examples/ and Rosetta/.
+  clock_gettime() with MONOTONIC timer is used for the measurement. 
+
+EOF
 }
 
-set_ukvm_permission
+measure_time() {
+  arg_benchdir=$1
+  # arg_apps_array=$2[@]
+  # arg_apps=("${!arg_apps_array}")
+  # arg_args_array=$3[@]
+  # arg_args=("${!arg_args_array}")
+  arg_repeat=$2
+  arg_ifile=$3
+  arg_ofile=$4
+
+  ### create dir where the results are saved
+  RESULTS_DIR="${EVAL_SCRIPT_ROOT}/time_$DATE"
+  mkdir -p ${RESULTS_DIR}
+  APPLIST_CSV="${EVAL_SCRIPT_ROOT}/${arg_ifile}"
+  RESULTS_CSV="${RESULTS_DIR}/${arg_ofile}"
+
+  ### add label to csv 
+  echo -n "app_name, " >> ${RESULTS_CSV}
+  for loop in $(seq 1 ${arg_repeat}); do
+    echo -n "${loop}, " >> ${RESULTS_CSV}
+  done
+  echo "average, stdev, " >> ${RESULTS_CSV}
+
+  ### execution
+  echo "move into ${arg_benchdir}..."
+  pushd ${arg_benchdir}
+
+  # python3 ${EVAL_SCRIPT_ROOT}/measure_time.py ${RESULTS_DIR} ${RESULTS_CSV} ${APPLIST_CSV} ${arg_repeat} ${UKVM_EXEC_CMD}
+  measure_ukvm_exec_time_py ${RESULTS_DIR} ${RESULTS_CSV} ${APPLIST_CSV} ${arg_repeat} ${UKVM_EXEC_CMD}
+
+  popd
+  echo "back to $(pwd)."
+}
+
+set_ukvm_permission # make ukvm-bin executable
+
+REPEAT=$1
+
+if [ -z ${REPEAT} ]; then
+  usage
+  exit -1
+fi
 
 DIR="time_$DATE"
 mkdir -p ${EVAL_SCRIPT_ROOT}/$DIR
 
-measure_time ${VITIS_EXAMPLES_DIR} VITIS_EXAMPLES_APPS VITIS_EXAMPLES_ARGS
-measure_time ${ROSETTA_DIR} ROSETTA_APPS ROSETTA_ARGS
+measure_time ${VITIS_EXAMPLES_DIR} ${REPEAT} "vitis_applist.csv" "vitis.csv"
+measure_time ${ROSETTA_DIR} ${REPEAT} "rosetta_applist.csv" "rosetta.csv"
 
