c4e5f76d238f5505d3c189becf89ef21bdcfed12
Felix Gust <gustifix@users.noreply.github.com>
2024-12-10 11:02:01 +0100

    Handle missing bitstreams in get_bitstream_freq.sh

--- funky-scripts/evaluation/get_bitstream_freq.sh
+++ funky-scripts/evaluation/get_bitstream_freq.sh
@@ -11,27 +11,29 @@ source ./common.sh
 
 vitis_bs_path=/share/felix/bitstreams/vitis-accel-examples
 rosetta_bs_path=/share/felix/bitstreams/rosetta-funky
-fpga_types="u50-300mhz u50-faster u280-300mhz u280-faster u280-ddr-300mhz u280-ddr-faster"
+fpga_types="u50-slow u50-fast u280-slow u280-fast u280-ddr-slow u280-ddr-fast"
 header="app_name,u50_slow_freq,u50_fast_freq,u280_slow_freq,u280_fast_freq,u280_ddr_slow_freq,u280_ddr_fast_freq"
+
+get_bitstream_freq() {
+  app_list=("$@")
+
+  for app in "${app_list[@]}"; do
+    echo -n "$app"
+    for fpga_type in $fpga_types; do
+      bs=$vitis_bs_path/$app/$fpga_type/bitstream
+      if [ -f "$bs" ]; then
+        output="$(xclbinutil --info --input "$vitis_bs_path"/"$app"/"$fpga_type"/bitstream \
+          | grep -A 4 ulp_ucs_aclk_kernel_00 | grep "Achieved Freq" | cut -d ":" -f 2 | cut -d " " -f 3)"
+      else
+        output=""
+      fi
+      echo -n ,"$output"
+    done
+    echo
+  done
+}
 
 echo $header
 
-for app in "${VITIS_EXAMPLES_APPS[@]}"; do
-  echo -n "$app"
-  for fpga_type in $fpga_types; do
-    output="$(xclbinutil --info --input "$vitis_bs_path"/"$app"/"$fpga_type"/bitstream \
-      | grep -A 4 ulp_ucs_aclk_kernel_00 | grep "Achieved Freq" | cut -d ":" -f 2 | cut -d " " -f 3)"
-    echo -n ,"$output"
-  done
-  echo
-done
-
-for app in "${ROSETTA_APPS[@]}"; do
-  echo -n "$app"
-  for fpga_type in $fpga_types; do
-    output="$(xclbinutil --info --input "$rosetta_bs_path"/"$app"/"$fpga_type"/bitstream \
-      | grep -A 4 ulp_ucs_aclk_kernel_00 | grep "Achieved Freq" | cut -d ":" -f 2 | cut -d " " -f 3)"
-    echo -n ,"$output"
-  done
-  echo
-done
+get_bitstream_freq "${VITIS_EXAMPLES_APPS[@]}"
+get_bitstream_freq "${ROSETTA_APPS[@]}"
