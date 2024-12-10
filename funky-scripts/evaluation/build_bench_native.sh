8ca3b93ff9b2b0f863b4dba7dad36077c9003388
Felix Gust <gustifix@users.noreply.github.com>
2024-12-10 15:06:40 +0100

    Add build script for native vitis acc ex

---
+++ funky-scripts/evaluation/build_bench_native.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./common.sh
+
+build_benchmark() {
+  arg_dir=$1
+  arg_apps_array=$2[@]
+  arg_apps=("${!arg_apps_array}")
+
+  for i in ${!arg_apps[@]}; do
+    echo "build ${arg_apps[$i]}..."
+    cd ${arg_dir}/${arg_apps[$i]}
+
+    make "${arg_apps[$i]}" &> /dev/null || echo "building ${arg_apps[$i]} failed" &
+  done 
+
+  for job in $(jobs -p); do
+    wait "$job"
+  done
+}
+
+build_benchmark /home/felix/Projects/vitis-accel-examples/ocl_kernels VITIS_EXAMPLES_APPS
+#build_benchmark ${ROSETTA_DIR} ROSETTA_APPS
