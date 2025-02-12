5f121e529cd51d0501d9cc250cfadb509748e99d
Felix Gust <gustifix@users.noreply.github.com>
2025-02-12 14:21:37 +0100

    Add include for cl_ext_xilinx.h to xcl2.hpp

--- examples/funky_utils/xcl2/xcl2.hpp
+++ examples/funky_utils/xcl2/xcl2.hpp
@@ -33,7 +33,7 @@
 #include <CL/opencl.hpp>
 
 // #include <CL/cl2.hpp>
-// #include <CL/cl_ext_xilinx.h>
+#include <CL/cl_ext_xilinx.h>
 #include <fstream>
 #include <iostream>
 // When creating a buffer with user pointer (CL_MEM_USE_HOST_PTR), under the
