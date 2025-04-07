915aee1321bb79d7f9567e5a00c5fda9420a3de1
Felix Gust <gustifix@users.noreply.github.com>
2025-04-07 14:24:18 +0200

    Adjust count_loc for Proteus

--- funky-scripts/evaluation/portability/count_loc.sh
+++ funky-scripts/evaluation/portability/count_loc.sh
@@ -30,7 +30,6 @@ count_loc() {
     fi
 
     COMMIT=`git rev-parse HEAD` # get commit hash
-    echo "${apps}, ${COMMIT}" >> ${CSV}
     # LOC of orig code
     # cloc --quiet ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/*.h --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file | tee ${LOG}
     # cloc --quiet ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/*.h --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file --csv >> ${CSV}
@@ -39,10 +38,10 @@ count_loc() {
       cp -ar ${arg_orig_dir}/${app}/imageLib/ ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/
     fi
 
-    cloc --quiet ${arg_dir}/${app}/${HOSTCODE} ${arg_dir}/${app}/*.h --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file | tee ${LOG}
-    cloc --quiet ${arg_dir}/${app}/${HOSTCODE} ${arg_dir}/${app}/*.h --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file --csv >> ${CSV}
+    # cloc --quiet ${arg_dir}/${app}/${HOSTCODE} ${arg_dir}/${app}/*.h --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file | tee ${LOG}
+    # cloc --quiet ${arg_dir}/${app}/${HOSTCODE} ${arg_dir}/${app}/*.h --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file --csv >> ${CSV}
 
-    echo "code changes for Funky" >> ${CSV}
+    # echo "code changes for Funky" >> ${CSV}
     cloc --quiet --diff ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_dir}/${app}/${HOSTCODE} --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file | tee ${LOG}
     cloc --quiet --diff ${arg_orig_dir}/${app}/${HOSTCODE_DIR}/${HOSTCODE} ${arg_dir}/${app}/${HOSTCODE} --include-lang=C/C++\ Header,C,C++ --exclude-dir=build --by-file --csv >> ${CSV}
 
@@ -60,12 +59,12 @@ count_loc() {
     # cloc --quiet ${arg_orig_dir}/../common/includes --exclude-dir=oclHelper,opencl,simplebmp --by-file | tee ${LOG}
     # cloc --quiet ${arg_orig_dir}/../common/includes --exclude-dir=oclHelper,opencl,simplebmp --by-file --csv >> ${CSV}
 
-    cloc --quiet ${arg_dir}/../funky_utils --exclude-dir=oclHelper,opencl,simplebmp --by-file | tee ${LOG}
-    cloc --quiet ${arg_dir}/../funky_utils --exclude-dir=oclHelper,opencl,simplebmp --by-file --csv >> ${CSV}
+    # cloc --quiet ${arg_dir}/../../funky_utils --exclude-dir=oclHelper,opencl,simplebmp --by-file | tee ${LOG}
+    # cloc --quiet ${arg_dir}/../../funky_utils --exclude-dir=oclHelper,opencl,simplebmp --by-file --csv >> ${CSV}
 
-    echo "code changes for Funky" >> ${CSV}
-    cloc --quiet --diff ${arg_orig_dir}/../common/includes ${arg_dir}/../funky_utils --exclude-dir=oclHelper,opencl,simplebmp,memdisk --by-file | tee ${LOG}
-    cloc --quiet --diff ${arg_orig_dir}/../common/includes ${arg_dir}/../funky_utils --exclude-dir=oclHelper,opencl,simplebmp,memdisk --by-file --csv >> ${CSV}
+    # echo "code changes for Funky" >> ${CSV}
+    cloc --quiet --diff ${arg_orig_dir}/../common/includes ${arg_dir}/../../funky_utils --exclude-dir=oclHelper,opencl,simplebmp,memdisk --by-file | tee ${LOG}
+    cloc --quiet --diff ${arg_orig_dir}/../common/includes ${arg_dir}/../../funky_utils --exclude-dir=oclHelper,opencl,simplebmp,memdisk --by-file --csv >> ${CSV}
   else 
     LOG="${arg_output_dir}/harness.log"
     CSV="${arg_output_dir}/harness.csv"
