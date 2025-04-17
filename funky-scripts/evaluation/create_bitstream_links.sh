9bc5850faa903e9ad819479a82c3b11c70d51e91
Felix Gust <gustifix@users.noreply.github.com>
2025-04-17 10:03:02 +0200

    Add Rosetta to create_bitstream_links.sh

--- funky-scripts/evaluation/create_bitstream_links.sh
+++ funky-scripts/evaluation/create_bitstream_links.sh
@@ -3,10 +3,22 @@
 set -u
  
 vitis_bs_path=/share/felix/bitstreams/vitis-accel-examples
-#rosetta_bs_path=/share/felix/bitstreams/rosetta-funky
+rosetta_bs_path=/share/felix/bitstreams/rosetta
 fpga_types="u50-slow u50-fast u280-slow u280-fast u280-ddr-slow u280-ddr-fast"
 
 for base_dir in "$vitis_bs_path"/*; do
+  for fpga_type in $fpga_types; do
+    bs_dir=$base_dir/$fpga_type
+    if [ -d "$bs_dir" ]; then
+      bs=$(find "$bs_dir" -name \*.xclbin -print | grep -v link)
+      if [ -f "$bs" ]; then
+        ln -sf "$(basename "$bs")" "$bs_dir"/bitstream
+      fi
+    fi
+  done
+done
+
+for base_dir in "$rosetta_bs_path"/*; do
   for fpga_type in $fpga_types; do
     bs_dir=$base_dir/$fpga_type
     if [ -d "$bs_dir" ]; then
