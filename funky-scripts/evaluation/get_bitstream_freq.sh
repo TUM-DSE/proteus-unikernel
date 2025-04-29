2fd927fcf5b91b5d94bc7a9004b78f83ad6f7d50
Felix Gust <gustifix@users.noreply.github.com>
2025-04-29 14:00:06 +0200

    Add Rosetta to get_bitstream_freq.sh

--- funky-scripts/evaluation/get_bitstream_freq.sh
+++ funky-scripts/evaluation/get_bitstream_freq.sh
@@ -10,19 +10,21 @@ source ./common.sh
 # Achieved Freq:  345 MHz
 
 vitis_bs_path=/share/felix/bitstreams/vitis-accel-examples
-rosetta_bs_path=/share/felix/bitstreams/rosetta-funky
+rosetta_bs_path=/share/felix/bitstreams/rosetta
 fpga_types="u50-slow u50-fast u280-slow u280-fast u280-ddr-slow u280-ddr-fast"
 header="app_name,u50_slow_freq,u50_fast_freq,u280_slow_freq,u280_fast_freq,u280_ddr_slow_freq,u280_ddr_fast_freq"
 
 get_bitstream_freq() {
+  bs_path=$1
+  shift 1
   app_list=("$@")
 
   for app in "${app_list[@]}"; do
     echo -n "$app"
     for fpga_type in $fpga_types; do
-      bs=$vitis_bs_path/$app/$fpga_type/bitstream
+      bs=$bs_path/$app/$fpga_type/bitstream
       if [ -f "$bs" ]; then
-        output="$(xclbinutil --info --input "$vitis_bs_path"/"$app"/"$fpga_type"/bitstream \
+        output="$(xclbinutil --info --input "$bs_path"/"$app"/"$fpga_type"/bitstream \
           | grep -A 4 ulp_ucs_aclk_kernel_00 | grep "Achieved Freq" | cut -d ":" -f 2 | cut -d " " -f 3)"
       else
         output=""
@@ -35,5 +37,5 @@ get_bitstream_freq() {
 
 echo $header
 
-get_bitstream_freq "${VITIS_EXAMPLES_APPS[@]}"
-get_bitstream_freq "${ROSETTA_APPS[@]}"
+get_bitstream_freq $vitis_bs_path "${VITIS_EXAMPLES_APPS[@]}"
+get_bitstream_freq $rosetta_bs_path "${ROSETTA_APPS[@]}"
