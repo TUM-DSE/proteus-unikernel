7a373f01c73d6986c294e7dd865600a6b413dd36
Felix Gust <gustifix@users.noreply.github.com>
2025-05-15 16:07:26 +0200

    Add Rosetta to ocl LoC script

--- funky-scripts/evaluation/portability/count_ocl_loc.sh
+++ funky-scripts/evaluation/portability/count_ocl_loc.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This script counts the number of OpenCL functions being called in
+# Vitis Accel Examples and Rosetta including their common libraries.
+
 set -euo pipefail
 
 script_dir=$(dirname "$(readlink -f "$0")")
@@ -12,13 +15,19 @@ vitis_common_files=("bitmap/bitmap.h" "bitmap/bitmap.cpp" "cmdparser/cmdlinepars
   "cmdparser/cmdlineparser.cpp" "logger/logger.h" "logger/logger.cpp" "xcl2/xcl2.hpp" \
   "xcl2/xcl2.cpp")
 
-include_pattern='cl::|\.setArg|\.finish|\.getProfilingInfo|\.enqueueMigrateMemObjects' \
-include_pattern+='|\..enqueueTask'
+rosetta_dir=$(realpath "$script_dir/../../../examples/Rosetta")
+
+include_pattern='cl::|\.setArg|\.finish|\.getProfilingInfo|\.enqueueMigrateMemObjects'
+include_pattern+='|\..enqueueTask|clFinish|clSetKernelArg|clCreateKernel|clReleaseKernel'
+include_pattern+='|clCreateBuffer|clEnqueueWriteBuffer|clGetEventProfilingInfo|clEnqueueReadBuffer'
+include_pattern+='clCreateProgramWithBinary|clEnqueueNDRangeKernel|clGetPlatformIDs|clGetDeviceIDs'
+include_pattern+='clGetDeviceInfo|clCreateContext|clCreateCommandQueue|clReleaseMemObject'
+include_pattern+='clReleaseProgram|clReleaseCommandQueue|clReleaseContext'
 exclude_pattern='xcl::'
 
 echo "app,ocl_loc"
 
-# Individual apps
+# Individual vitis apps
 for app in "${vitis_apps[@]}"; do
   echo -n "$app,"
   grep -E "$include_pattern" "$vitis_dir/$app/host.cpp" | grep -Ev "$exclude_pattern" -c || true
@@ -31,3 +40,15 @@ for file in "${vitis_common_files[@]}"; do
   sum=$((sum + loc))
 done
 echo "vitis_common_lib,$sum"
+
+# Rosetta
+echo -n "3d-rendering,"
+grep -E "$include_pattern" "$rosetta_dir"/3d-rendering/{*.h,*.cpp} | grep -Ev "$exclude_pattern" -c || true
+echo -n "digit-recognition,"
+grep -E "$include_pattern" "$rosetta_dir"/digit-recognition/{*.h,*.cpp} | grep -Ev "$exclude_pattern" -c || true
+echo -n "optical-flow,"
+grep -E "$include_pattern" "$rosetta_dir"/optical-flow/{*.h,*.cpp} | grep -Ev "$exclude_pattern" -c || true
+echo -n "spam-filter,"
+grep -E "$include_pattern" "$rosetta_dir"/spam-filter/{*.h,*.cpp} | grep -Ev "$exclude_pattern" -c || true
+echo -n "rosetta_common_lib,"
+grep -E "$include_pattern" "$rosetta_dir"/harness/{*.h,*.cpp} | grep -Ev "$exclude_pattern" -c || true
