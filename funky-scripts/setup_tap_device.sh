94f69d31e9a5541ee03b7c199bf9d40228a9bcd2
Felix Gust <gustifix@users.noreply.github.com>
2026-02-04 13:07:33 +0100

    Update setup_tap_device.sh

--- funky-scripts/setup_tap_device.sh
+++ funky-scripts/setup_tap_device.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sudo ip tuntap add tap100 mode tap user $USER
-sudo ip link set dev tap100 up
-sudo ip addr add 10.0.0.1/24 dev tap100
+dev=tap100
+
+if ! ip link show "$dev" &>/dev/null; then
+  sudo ip tuntap add "$dev" mode tap user "$USER"
+  sudo ip link set dev "$dev" up
+  sudo ip addr add 10.0.0.1/24 dev "$dev"
+fi
