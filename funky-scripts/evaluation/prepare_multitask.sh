836eb6f8bcf6ad8623898d16a8ddb31f52e6d853
Felix Gust <gustifix@users.noreply.github.com>
2026-02-10 12:23:15 +0100

    Fix branch names for multi-fpga evaluation

--- funky-scripts/evaluation/prepare_multitask.sh
+++ funky-scripts/evaluation/prepare_multitask.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 script_dir=$(dirname "$(readlink -f "$0")")
 sched_src_dir=$script_dir/../../../funky-monitor/scheduler
 sched_bin_dir=$script_dir/sched_bins
-branches="single_u50 single_u280 2_u50 2_u280 2_u280_second_instance 4_u50 4_u280 4_u280_second_instance"
+branches="1_fpga_u50 1_fpga_u280 2_fpga_50 2_fpga_280 4_fpga"
 
 pushd "$sched_src_dir"
 
