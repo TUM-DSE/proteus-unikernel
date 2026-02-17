e9dae48d7cd422355af141db8944c04e258a1637
Felix Gust <gustifix@users.noreply.github.com>
2026-02-17 15:09:51 +0100

    Update setup_tap_device.sh

--- funky-scripts/setup_tap_device.sh
+++ funky-scripts/setup_tap_device.sh
@@ -3,7 +3,7 @@
 dev=tap100
 
 if ! ip link show "$dev" &>/dev/null; then
-  sudo ip tuntap add "$dev" mode tap user "$USER"
+  sudo ip tuntap add "$dev" mode tap
   sudo ip link set dev "$dev" up
   sudo ip addr add 10.0.0.1/24 dev "$dev"
 fi
@@ -11,7 +11,7 @@ fi
 dev=tap0
 
 if ! ip link show "$dev" &>/dev/null; then
-  sudo ip tuntap add "$dev" mode tap user "$USER"
+  sudo ip tuntap add "$dev" mode tap
   sudo ip link set dev "$dev" up
   sudo ip addr add 10.0.1.1/24 dev "$dev"
 fi
@@ -19,7 +19,7 @@ fi
 dev=tap1
 
 if ! ip link show "$dev" &>/dev/null; then
-  sudo ip tuntap add "$dev" mode tap user "$USER"
+  sudo ip tuntap add "$dev" mode tap
   sudo ip link set dev "$dev" up
   sudo ip addr add 10.0.2.1/24 dev "$dev"
 fi
